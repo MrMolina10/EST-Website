d2729545fd5ec56f49d824d2f0fe6f59a80273bc
MrMolina10 <roberto.molina126@gmail.com>
2023-08-06 14:56:26 -0400

    Draft Home Page

---
+++ app.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('preloader-finish');
+});
+
+
+let isModalOpen = false;
+          const swiper = new Swiper(".swiper", {
+            autoplay: {
+              delay: 2000,
+              disableOnInteraction: false,
+            },
+            loop: true,
+    
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+    
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
